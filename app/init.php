bef9df03b826d4a98a792b4065084e1f3cb2e562
henyumino <henyumino37@gmail.com>
2019-05-13 18:56:18 +0800

    fixing autoload

--- app/init.php
+++ app/init.php
@@ -1,5 +1,8 @@
 <?php
 
-spl_autoload_register(function ($class_name) {
-    require_once '../app/core/'.$class_name . '.php';
-});
+require_once 'core/App.php';
+require_once 'core/Controller.php';
+require_once 'core/Database.php';
+require_once 'core/Flasher.php';
+
+require_once 'config/config.php';
